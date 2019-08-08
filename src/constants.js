722773a86d65733a53bef674ab64150dcd3eb1ab
Joost van Someren <joost.v.someren@gmail.com>
2019-08-08 21:58:48 +0200

    add event constants and update defaults

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const PEER_EVENTS = [
+  'open',
+  'connection',
+  'call',
+  'close',
+  'disconnected',
+  'error',
+];
+
+export const DATA_CONNECTION_EVENTS = [
+  'data',
+  'open',
+  'close',
+  'error',
+];
+
+export const MEDIA_CONNECTION_EVENTS = [
+  'stream',
+  'close',
+  'error',
+];
--- src/defaults.js
+++ src/defaults.js
@@ -1,4 +1,8 @@
+import camelcase from 'camelcase';
+
 export default Object.freeze({
   actionPrefix: 'peer_',
   mutationPrefix: 'PEER_',
+  eventToMutationTransformer: event => event.toUpperCase(),
+  eventToActionTransformer: camelcase,
 });
