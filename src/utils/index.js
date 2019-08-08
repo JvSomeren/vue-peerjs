3cd7577a4948c8e16bf185f374e0ca96db2ab035
Joost van Someren <joost.v.someren@gmail.com>
2019-08-08 21:59:17 +0200

    add utils
    unwrapIfSingle
    pipe
    prefixWith
    augmentMethod

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,26 @@ export const isFunction = obj => (
 export const isPeerJS = obj => (
   !!obj && isFunction(obj.on) && isFunction(obj.call)
 );
+
+export const unwrapIfSingle = args => (
+  args && args.length <= 1
+    ? args[0]
+    : args
+);
+
+export const pipe = (...fns) => x => (
+  fns.reduce((v, f) => f(v), x)
+);
+
+export const prefixWith = prefix => string => (
+  prefix + string
+);
+
+export const augmentMethod = (obj, methodKey, cb) => {
+  const originalFn = obj[methodKey];
+  // eslint-disable-next-line no-param-reassign
+  obj[methodKey] = (...args) => {
+    originalFn.call(obj, ...args);
+    cb(...args);
+  };
+};
